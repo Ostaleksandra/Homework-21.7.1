fa6ea8d95b003dfc71789caf46d239a4d500e977
Ostaleksandra <123018725+Ostaleksandra@users.noreply.github.com>
2023-04-03 02:24:00 +0300

    Homework 21.7.1

---
+++ Homework 21.7.1/Cat.py
@@ -0,0 +1,9 @@
+from PetHome_Cat import Cat
+
+p1 = Cat("Барон", "мальчик", 2)
+p2 = Cat("Сэм", "мальчик", 2)
+
+print("Имя первого кота:", p1.getName())
+print("Имя первого кота:", p2.getName())
+print("Возраст первого кота:", p1.getAge())
+print("Возраст второго кота:", p2.getAge())
